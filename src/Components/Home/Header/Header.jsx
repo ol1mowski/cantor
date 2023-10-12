7a27da3e63f81ed3107f8ee3a5aca34503431537
Ol1mowski <olimarki123@wp.pl>
2023-10-12 10:07:00 +0200

    add rwd to header to 1024px

--- src/Components/Home/Header/Header.jsx
+++ src/Components/Home/Header/Header.jsx
@@ -6,10 +6,30 @@ const Header = () => {
             <header className={style.headerContainer}>
                 <div className={style.headerContainer__img} ></div>
                 <div className={style.headerContainer__right}>
-
-                    <div className={style.headerContainer__login}>
-                        <a className={style.headerContainer__login__a} href='/login'>Zaloguj się</a>
-                    </div>
+                    <section className={style.headerContainer__menuBar}>
+                        <div className={style.headerContainer__menuBar__wrapper}>
+                            <div className={style.headerContainer__menuBar__wrapper__items}>
+                                Usługi
+                            </div>
+                            <div className={style.headerContainer__menuBar__wrapper__items}>
+                                Aktualności
+                            </div> <div className={style.headerContainer__menuBar__wrapper__items}>
+                                Portfel walutowy
+                            </div>
+                        </div>
+                    </section>
+                    <section className={style.headerContainer__loginSection}>
+                        <div className={style.headerContainer__login}>
+                            <a className={style.headerContainer__login__a} href='/login'>Zaloguj się</a>
+                        </div>
+                        <div className={style.headerContainer__register}>
+                            <a className={style.headerContainer__register__a} href='/reg'>
+                                <button className={style.headerContainer__register__btn}>
+                                    Zarejestruj się za darmo
+                                </button>
+                            </a>
+                        </div>
+                    </section>
                     <nav className={style.headerContainer__menu}>
                         Menu
                     </nav>
