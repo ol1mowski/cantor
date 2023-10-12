40b0c95d21780500db876579f0323fc50bcbdb44
Ol1mowski <olimarki123@wp.pl>
2023-10-12 10:22:44 +0200

    add sketch rwd to main to 1024px

--- src/Components/Home/Main/Main.jsx
+++ src/Components/Home/Main/Main.jsx
@@ -6,15 +6,17 @@ const Main = () => {
     return (
         <>
             <main className={style.mainContainer}>
-                <h1 className={style.mainContainer__h1}>
-                    Wielowalutowe usługi finansowe
-                </h1>
-                <p className={style.mainContainer__p}>
-                    Zacznij oszczędzać czas i pieniądze.<br /> Skorzystaj z wygodnych usług walutowych.
-                </p>
-                <button className={style.mainContainer__button}>
-                    Zarejestruj się za darmo
-                </button>
+                <section className={style.mainContainer__contentContainer}>
+                    <h1 className={style.mainContainer__h1}>
+                        Wielowalutowe usługi finansowe
+                    </h1>
+                    <p className={style.mainContainer__p}>
+                        Zacznij oszczędzać czas i pieniądze.<br /> Skorzystaj z wygodnych usług walutowych.
+                    </p>
+                    <button className={style.mainContainer__button}>
+                        Zarejestruj się za darmo
+                    </button>
+                </section>
                 <img className={style.mainContainer__animate} src={animate} alt='animation' />
             </main>
             <Cards />
